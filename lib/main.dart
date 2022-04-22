1d692832064d11332f85749f6654407b29f58381
ik-top-digital <top.digital.ik@gmail.com>
2022-04-22 11:30:21 +0300

    exploring alternative way for provider syntaxes

--- lib/main.dart
+++ lib/main.dart
@@ -16,8 +16,8 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return ChangeNotifierProvider(
-      create: ((context) => ProductsProvider() ),
+    return ChangeNotifierProvider.value(
+      value: ProductsProvider() ,
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         title: 'Flutter Shop App',
--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -14,8 +14,9 @@ class ProductsGrid extends StatelessWidget {
     return GridView.builder(
       padding: const EdgeInsets.all(10.0),
       itemCount: products.length,
-      itemBuilder: (ctx, i) => ChangeNotifierProvider(
-        create: (BuildContext context) => products[i],
+      itemBuilder: (ctx, i) => ChangeNotifierProvider.value(
+        // create: (BuildContext context) => products[i],
+        value: products[i],
         child: ProductItem(
           // products[i].id,
           // products[i].title,
